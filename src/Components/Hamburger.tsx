768819ee19100fb023e0bde1fb4143cbd7189117
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-07 15:13:37 +0400

    faet: add services section on menu bar

--- src/Components/Hamburger.tsx
+++ src/Components/Hamburger.tsx
@@ -16,7 +16,28 @@ function Hamburger() {
             }}
           ></div>
           <div className="pl-[8px] pr-[13px] pt-[8px] pb-[36px] flex absolute right-0 z-10 flex-row-reverse">
-            <div className="bg-[#FFFFFF] rounded-[10px] h-[590px] w-[350px]"></div>
+            <div className="bg-[#FFFFFF] rounded-[10px] h-[590px] w-[350px]">
+              <div className="pt-[10px]">
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">Home</p>
+                </div>
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">About</p>
+                </div>
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">Services</p>
+                </div>
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">Projects</p>
+                </div>
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">Team</p>
+                </div>
+                <div className="px-[15px] py-[20px] hover:bg-[#FFC451] duration-300 ease-in-out font-normal h-[42px] flex items-center cursor-pointer">
+                  <p className="text-[17px]">Contact</p>
+                </div>
+              </div>
+            </div>
           </div>
         </>
       )}
