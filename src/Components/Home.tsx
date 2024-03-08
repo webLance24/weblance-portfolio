74def86a26d5e7d3ceeba26fa36cf12bc3ba7f9d
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-08 22:51:21 +0400

    feat: add home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,14 +1,48 @@
 // import { useContext } from "react";
 // import { MyContext } from "./Context";
+import imgOne from "../../public/img/market.png";
 
 function Home() {
   // const context = useContext(MyContext);
   // const {}: any = context;
 
   return (
-    <div>
-      <h1>Powerful Websites with webLance</h1>
-      <p>We are team of talented digital developers</p>
+    // here is home page
+    <div className="pt-[75px] px-[12px] bg-[black] bg-opacity-60">
+      <h1 className="text-[white] text-[40px] font-bold text-center">
+        Powerful Websites with webLance
+      </h1>
+      <p className="text-[white] text-[20px] font-light text-center">
+        We are team of talented digital developers
+      </p>
+      {/* Containers section */}
+      <div className="flex flex-col gap-[20px] justify-center items-center mt-[40px]">
+        {/* First div */}
+        <div className="w-[100%] h-[130px] border-[gray] border-[2px] flex flex-col justify-center items-center">
+          <img className="w-[32px]" src={imgOne} alt="market image here" />
+          <h3 className="text-[20px] text-[white]">Lorem Ipsum</h3>
+        </div>
+        {/* Second div */}
+        <div className="w-[100%] h-[130px] border-[gray] border-[2px] flex flex-col justify-center items-center">
+          <img className="w-[32px]" src={imgOne} alt="market image here" />
+          <h3 className="text-[20px] text-[white]">Lorem Ipsum</h3>
+        </div>
+        {/* third div */}
+        <div className="w-[100%] h-[130px] border-[gray] border-[2px] flex flex-col justify-center items-center">
+          <img className="w-[32px]" src={imgOne} alt="market image here" />
+          <h3 className="text-[20px] text-[white]">Lorem Ipsum</h3>
+        </div>
+        {/* Fourth div */}
+        <div className="w-[100%] h-[130px] border-[gray] border-[2px] flex flex-col justify-center items-center">
+          <img className="w-[32px]" src={imgOne} alt="market image here" />
+          <h3 className="text-[20px] text-[white]">Lorem Ipsum</h3>
+        </div>
+        {/* Sixth div */}
+        <div className="w-[100%] h-[130px] border-[gray] border-[2px] flex flex-col justify-center items-center">
+          <img className="w-[32px]" src={imgOne} alt="market image here" />
+          <h3 className="text-[20px] text-[white]">Lorem Ipsum</h3>
+        </div>
+      </div>
     </div>
   );
 }
