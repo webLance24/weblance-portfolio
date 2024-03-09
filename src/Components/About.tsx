99f7724f3e744d53a7c6926787c63a9489b9cd7e
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-09 23:54:59 +0400

    feat: add slider

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,20 +1,34 @@
 import aboutImg from "../../public/img/about.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function About() {
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
     <div className="bg-[white] pt-[50px] px-[12px] flex justify-center flex-col items-center">
       <img className="w-[100%] h-[100%]" src={aboutImg} alt="" />
-      <h3>
-        Lorem ipsum is placeholder text commonly used in the graphic, print, and
-        publishing industries for previewing layouts and visual mockups. LOREM
-        IPSUM GENERATOR
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
+      {/* text section */}
+      <div className="py-[24px]">
+        <h3 className="text-[#444444] text-[28px] font-bold">
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing LOREM IPSUM GENERATOR
+        </h3>
+        <p className="text-[16px] font-normal text-[#444444]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <Slider {...settings}></Slider>
     </div>
   );
 }
