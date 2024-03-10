1cd0654a96f5356618757cbcf512e79ee5b18f29
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-10 16:07:26 +0400

    feat: add about section

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,24 +1,13 @@
 import aboutImg from "../../public/img/about.jpg";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import client1 from "../../public/img/clients/client-1.png";
-import client2 from "../../public/img/clients/client-2.png";
-import client3 from "../../public/img/clients/client-3.png";
-import client4 from "../../public/img/clients/client-4.png";
-import client5 from "../../public/img/clients/client-5.png";
-import client6 from "../../public/img/clients/client-6.png";
+import boo from "../../public/img/boo.png";
+// import client1 from "../../public/img/clients/client-1.png";
+// import client2 from "../../public/img/clients/client-2.png";
+// import client3 from "../../public/img/clients/client-3.png";
+// import client4 from "../../public/img/clients/client-4.png";
+// import client5 from "../../public/img/clients/client-5.png";
+// import client6 from "../../public/img/clients/client-6.png";
 
 function About() {
-  const settings = {
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true,
-  };
-
   return (
     <div className="bg-[white] pt-[50px] px-[12px] flex justify-center flex-col items-center">
       <img className="w-[100%] h-[100%]" src={aboutImg} alt="" />
@@ -28,22 +17,71 @@ function About() {
           Lorem ipsum is placeholder text commonly used in the graphic, print,
           and publishing LOREM IPSUM GENERATOR
         </h3>
-        <p className="text-[16px] font-normal text-[#444444]">
+        <p className="text-[16px] font-normal text-[#444444] mt-[20px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
       </div>
-      <div className="slider-container">
-        <Slider {...settings}>
-          <img src={client1} alt="" />
-          <img src={client2} alt="" />
-          <img src={client3} alt="" />
-          <img src={client4} alt="" />
-          <img src={client5} alt="" />
-          <img src={client6} alt="" />
-        </Slider>
+      {/* <div className="flex justify-center items-center">
+        <img src={client1} alt="" style={{ width: "100px" }} />
+        <img src={client2} alt="" style={{ width: "100px" }} />
+        <img src={client3} alt="" style={{ width: "100px" }} />
+      </div> */}
+      <div className="flex justify-center flex-col items-center mt-[10px]">
+        {/* first container */}
+        <div className="flex">
+          <img src={boo} alt="" className="w-[70px] h-[100%]" />
+          <div>
+            <h4 className="text-[20px] font-bold text-[#464646]">
+              Lorem ipsum
+            </h4>
+            <p className="text-[15px] font-normal text-[#464646] mt-[7px]">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum
+            </p>
+          </div>
+        </div>
+        {/* second container */}
+        <div className="flex mt-[48px]">
+          <img src={boo} alt="" className="w-[70px] h-[100%]" />
+          <div>
+            <h4 className="text-[20px] font-bold text-[#464646]">
+              Lorem ipsum
+            </h4>
+            <p className="text-[15px] font-normal text-[#464646] mt-[7px]">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum
+            </p>
+          </div>
+        </div>
+        {/* third container */}
+        <div className="flex mt-[48px]">
+          <img src={boo} alt="" className="w-[70px] h-[100%]" />
+          <div>
+            <h4 className="text-[20px] font-bold text-[#464646]">
+              Lorem ipsum
+            </h4>
+            <p className="text-[15px] font-normal text-[#464646] mt-[7px]">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum
+            </p>
+          </div>
+        </div>
+        {/* fourth container */}
+        <div className="flex mt-[48px]">
+          <img src={boo} alt="" className="w-[70px] h-[100%]" />
+          <div>
+            <h4 className="text-[20px] font-bold text-[#464646]">
+              Lorem ipsum
+            </h4>
+            <p className="text-[15px] font-normal text-[#464646] mt-[7px]">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
