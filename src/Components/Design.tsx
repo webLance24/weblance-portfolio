36ac8cbcf4359011516d79f356e5baf600651260
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-17 02:30:16 +0400

    feat: add social icon on projects

--- src/Components/Design.tsx
+++ src/Components/Design.tsx
@@ -1,4 +1,6 @@
 import data from "../../data.json";
+import github from "../../public/img/social/github-white.png";
+import link from "../../public/img/social/link.png";
 
 const designImages = (
   data.photos.find((category) => category.category === "design") || {
@@ -16,11 +18,27 @@ function Design() {
               key={index}
               src={image.src}
               alt={image.title}
-              className="image block w-[100%] h-auto"
+              className="block w-[100%] h-auto"
               style={{ width: "100%" }}
             />
-            <div className="middle">
-              <div className="text">{image.title}</div>
+            {/* live links section */}
+            <div className="flex">
+              {/* github section */}
+              <div className="middle w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                <img
+                  src={github}
+                  alt="Github"
+                  className="w-[30px] h-[30px] mx-1"
+                />
+              </div>
+              {/* link section */}
+              <div className="middle ml-[70px] w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                <img
+                  src={link}
+                  alt="Github"
+                  className="w-[30px] h-[30px] mx-1"
+                />
+              </div>
             </div>
           </div>
         ))}
