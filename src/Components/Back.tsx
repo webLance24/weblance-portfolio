f2615def7936b5f777bf63c1de6ba3018ca93f38
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-17 14:24:29 +0400

    feat: add social links by hover on projects

--- src/Components/Back.tsx
+++ src/Components/Back.tsx
@@ -1,6 +1,8 @@
 import data from "../../data.json";
+import github from "../../public/img/social/github-white.png";
+import link from "../../public/img/social/link.png";
 
-const backImages = (
+const designImages = (
   data.photos.find((category) => category.category === "back-end") || {
     images: [],
   }
@@ -10,13 +12,35 @@ function Back() {
   return (
     <div className="bg-[white] px-[12px]">
       <div>
-        {backImages.map((image, index) => (
-          <img
-            className="pb-[15px]"
-            key={index}
-            src={image.src}
-            alt={image.title}
-          />
+        {designImages.map((image, index) => (
+          <div className="main pb-[15px]" key={index}>
+            <img
+              key={index}
+              src={image.src}
+              alt={image.title}
+              className="block w-[100%] h-auto"
+              style={{ width: "100%" }}
+            />
+            {/* live links section */}
+            <div className="flex">
+              {/* github section */}
+              <div className="middle w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                <img
+                  src={github}
+                  alt="Github"
+                  className="w-[30px] h-[30px] mx-1"
+                />
+              </div>
+              {/* link section */}
+              <div className="middle ml-[70px] w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                <img
+                  src={link}
+                  alt="Github"
+                  className="w-[30px] h-[30px] mx-1"
+                />
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
