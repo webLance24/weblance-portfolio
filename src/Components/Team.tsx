86df1e3198de054c95580cd696c2bfcd393eda4a
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-25 18:39:29 +0400

    feat: add responsive for tablet team

--- src/Components/Team.tsx
+++ src/Components/Team.tsx
@@ -5,9 +5,44 @@ import githubIcon from "../../public/img/social/github-profile.png";
 import linkedinIcon from "../../public/img/social/linkedin-profile.png";
 
 function Team() {
+  const categories = [
+    {
+      name: "Giorgi Mamulashvili",
+      category: "CEO & Founder",
+      src: gio,
+      insta: instagramIcon,
+      github: githubIcon,
+      linkedin: linkedinIcon,
+    },
+    {
+      name: "David Mikeladze",
+      category: "CTO & Co-Founder",
+      src: gio,
+      insta: instagramIcon,
+      github: githubIcon,
+      linkedin: linkedinIcon,
+    },
+    {
+      name: "David Mikeladze",
+      category: "CTO & Co-Founder",
+      src: gio,
+      insta: instagramIcon,
+      github: githubIcon,
+      linkedin: linkedinIcon,
+    },
+    {
+      name: "David Mikeladze",
+      category: "CTO & Co-Founder",
+      src: gio,
+      insta: instagramIcon,
+      github: githubIcon,
+      linkedin: linkedinIcon,
+    },
+  ];
+
   return (
     <div id="team" className="bg-[white] px-[12px]">
-      <div className="bg-[white] pt-[120px] max-w-[520px] m-auto md:max-w-[696px]">
+      <div className="bg-[white] pt-[120px] max-w-[520px] m-auto md:max-w-[696px] lg:max-w-[948px]">
         <div className="flex items-center gap-[10px]">
           <h2 className="uppercase text-[#AAAAAA] text-[15px] tracking-[3px]">
             Team
@@ -19,126 +54,80 @@ function Team() {
         </p>
       </div>
       {/* photo section */}
-      <div className="mt-[40px] md:max-w-[670px] m-auto md:gap-[40px] grid grid-cols-1 md:grid-cols-2 place-items-center">
+      <div className="mt-[40px] md:max-w-[670px] m-auto md:gap-[40px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center lg:max-w-[948px]">
         {/* Giorgi */}
-        <div className="mt-[30px] max-w-[520px] md:w-[336px]">
-          <div className="relative flex justify-center items-center container">
-            <img
-              src={gio}
-              alt="Giorgi Mamulashvilis Photo"
-              className="rounded-t-lg block w-[100%] h-auto"
-            />
-            {/* Social network icons */}
-            <div className="overlay flex gap-[15px] absolute bottom-[30px] opacity-0 hover:opacity-100 transition-opacity duration-300">
-              {/* instagram section */}
-              <div>
-                <a
-                  href="https://www.instagram.com/giorgi_mamulashvil1/"
-                  target="_blank"
-                >
-                  <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                    <img
-                      src={instagramIcon}
-                      alt="Instagram"
-                      className="w-[30px] h-[30px] mx-1"
-                    />
-                  </div>
-                </a>
-              </div>
-              {/* github section */}
-              <div>
-                <a href="https://github.com/gmamulashvili10" target="_blank">
-                  <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                    <img
-                      src={githubIcon}
-                      alt="Github"
-                      className="w-[30px] h-[30px] mx-1"
-                    />
-                  </div>
-                </a>
-              </div>
-              {/* linkedin section */}
-              <div>
-                <a
-                  href="https://www.linkedin.com/in/giorgi-mamulashvili-0a7883261/"
-                  target="_blank"
-                >
-                  <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                    <img
-                      src={linkedinIcon}
-                      alt="LinkedIn"
-                      className="w-[30px] h-[30px] mx-1"
-                    />
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="px-[15px] py-[25px] border-[2px] rounded-b-lg">
-            <h4 className="text-[19px] text-[black] font-semibold">
-              Giorgi Mamulashvili
-            </h4>
-            <p className="text-[15px] font-normal text-[#464646]">
-              CEO & Founder
-            </p>
-          </div>
-        </div>
-        {/* David */}
-        <div className="mt-[30px] max-w-[520px] md:w-[336px]">
-          <div className="relative flex justify-center items-center container">
-            <img
-              src={gio}
-              alt="David Mikeladzes Photo"
-              className="rounded-t-lg block w-[100%] h-auto"
-            />
-            {/* Social network icons */}
-            <div className="overlay flex gap-[15px] absolute bottom-[30px] opacity-0 hover:opacity-100 transition-opacity duration-300">
-              {/* instagram section */}
-              <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+        {categories.map((category, index) => {
+          return (
+            <div
+              className="mt-[30px] max-w-[520px] md:w-[336px] lg:w-[216px]"
+              key={index}
+            >
+              <div className="relative flex justify-center items-center container">
                 <img
-                  src={instagramIcon}
-                  alt="Instagram"
-                  className="w-[30px] h-[30px] mx-1"
+                  src={category.src}
+                  alt="Giorgi Mamulashvilis Photo"
+                  className="rounded-t-lg block w-[100%] h-auto"
                 />
-              </div>
-              {/* github section */}
-              <div>
-                <a href="https://github.com/Davmike" target="_blank">
-                  <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                    <img
-                      src={githubIcon}
-                      alt="Github"
-                      className="w-[30px] h-[30px] mx-1"
-                    />
+                {/* Social network icons */}
+                <div className="overlay flex gap-[15px] absolute bottom-[30px] opacity-0 hover:opacity-100 transition-opacity duration-300">
+                  {/* instagram section */}
+                  <div>
+                    <a
+                      href="https://www.instagram.com/giorgi_mamulashvil1/"
+                      target="_blank"
+                    >
+                      <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                        <img
+                          src={category.insta}
+                          alt="Instagram"
+                          className="w-[30px] h-[30px] mx-1"
+                        />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-              {/* linkedin section */}
-              <div>
-                <a
-                  href="https://www.linkedin.com/in/david-mikeladze-927406264/"
-                  target="_blank"
-                >
-                  <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                    <img
-                      src={linkedinIcon}
-                      alt="LinkedIn"
-                      className="w-[30px] h-[30px] mx-1"
-                    />
+                  {/* github section */}
+                  <div>
+                    <a
+                      href="https://github.com/gmamulashvili10"
+                      target="_blank"
+                    >
+                      <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                        <img
+                          src={category.github}
+                          alt="Github"
+                          className="w-[30px] h-[30px] mx-1"
+                        />
+                      </div>
+                    </a>
                   </div>
-                </a>
+                  {/* linkedin section */}
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/giorgi-mamulashvili-0a7883261/"
+                      target="_blank"
+                    >
+                      <div className="w-[55px] h-[55px] bg-[#ffffffd3] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                        <img
+                          src={category.linkedin}
+                          alt="LinkedIn"
+                          className="w-[30px] h-[30px] mx-1"
+                        />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="px-[15px] py-[25px] border-[2px] rounded-b-lg">
+                <h4 className="text-[19px] text-[black] font-semibold">
+                  {category.name}
+                </h4>
+                <p className="text-[15px] font-normal text-[#464646]">
+                  {category.category}
+                </p>
               </div>
             </div>
-          </div>
-          <div className="px-[15px] py-[25px] border-[2px] rounded-b-lg">
-            <h4 className="text-[19px] text-[black] font-semibold">
-              David Mikeladze
-            </h4>
-            <p className="text-[15px] font-normal text-[#464646]">
-              CTO & Co-Founder
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
