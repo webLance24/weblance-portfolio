3f792e4028a964d3eb307a8623dfb2d96976329e
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-25 17:58:43 +0400

    feat: add responsive for tablet services

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -3,9 +3,42 @@ import front from "../../public/img/icons/front.png";
 import back from "../../public/img/icons/back.png";
 
 function Services() {
+  const categories = [
+    {
+      title: "Design",
+      category: "lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: design,
+    },
+    {
+      title: "Front-end",
+      category: " lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: front,
+    },
+    {
+      title: " Back-end",
+      category: "lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: back,
+    },
+    {
+      title: "Back-end",
+      category: "lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: back,
+    },
+    {
+      title: "Back-end",
+      category: "lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: back,
+    },
+    {
+      title: "Back-end",
+      category: "lorem ipsum ipsum lorem lorem ipsum ipsum lorem",
+      src: back,
+    },
+  ];
+
   return (
     <div id="services" className="bg-[white]">
-      <div className="bg-[white] pt-[120px] px-[12px] max-w-[520px] m-auto md:max-w-[696px]">
+      <div className="bg-[white] pt-[120px] px-[12px] max-w-[520px] m-auto md:max-w-[696px] lg:max-w-[948px]">
         <div className="pb-[40px]">
           <div className="flex items-center gap-[10px]">
             <h2 className="uppercase text-[#AAAAAA] text-[15px] tracking-[3px]">
@@ -17,64 +50,32 @@ function Services() {
             Check our services
           </p>
         </div>
-        {/* main container */}
-        <div className="md:flex md:flex-row md:justify-center md:items-center md:max-w-[696px] md:gap-[20px]">
-          {/* design container */}
-          <div className="md:flex md:gap-[20px] md:flex-col">
-            {/* for 2 div up div means */}
-            <div className=" flex items-center justify-center flex-col px-[20px] py-[80px] border-[2px] md:w-[326px] md:h-[337px]">
-              <div className="w-[100%] h-[175px] flex flex-col justify-center items-center md:w-[294px]">
-                <div className="w-[64px] h-[64px] bg-[#FFC451] rounded-[5px] flex justify-center items-center">
-                  <img className="w-[28px] h-[28px]" src={design} alt="" />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-y-[15px]">
+          {categories.map((category, index) => {
+            return (
+              <div
+                key={index}
+                className="flex items-center justify-center mt-[10px] flex-col px-[20px] py-[80px] border-[2px] md:w-[326px] md:h-[337px] lg:mt-0 lg:w-[295px]"
+              >
+                <div className="w-[100%] h-[175px] flex flex-col justify-center items-center md:w-[294px]">
+                  <div className="w-[64px] h-[64px] bg-[#FFC451] rounded-[5px] flex justify-center items-center">
+                    <img
+                      className="w-[28px] h-[28px]"
+                      src={category.src}
+                      alt=""
+                    />
+                  </div>
+                  <h4 className="text-[black] text-[24px] mt-[10px]">
+                    {category.title}
+                  </h4>
+                  <p className="text-[#444444] text-[16px] font-normal text-center">
+                    {category.category}
+                  </p>
                 </div>
-                <h4 className="text-[black] text-[24px] mt-[10px]">Design</h4>
-                <p className="text-[#444444] text-[16px] font-normal text-center">
-                  lorem ipsum ipsum lorem lorem ipsum ipsum lorem
-                </p>
               </div>
-            </div>
-            {/* front-end container */}
-            <div className=" flex items-center justify-center flex-col px-[20px] py-[80px] mt-[40px] border-[2px] md:w-[326px] md:h-[337px] md:mt-0">
-              <div className="w-[100%] h-[175px] flex flex-col justify-center items-center md:w-[294px]">
-                <div className="w-[64px] h-[64px] bg-[#FFC451] rounded-[5px] flex justify-center items-center">
-                  <img className="w-[28px] h-[28px]" src={front} alt="" />
-                </div>
-                <h4 className="text-[black] text-[24px] mt-[10px]">
-                  Front-end
-                </h4>
-                <p className="text-[#444444] text-[16px] font-normal text-center">
-                  lorem ipsum ipsum lorem lorem ipsum ipsum lorem
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* back-end container */}
-          <div className="md:flex md:gap-[20px] md:flex-col">
-            {/* for 2 div up div means */}
-            <div className=" flex items-center justify-center flex-col px-[20px] py-[80px] mt-[40px] border-[2px] md:w-[326px] md:h-[337px] md:mt-0">
-              <div className="w-[100%] h-[175px] flex flex-col justify-center items-center md:w-[294px]">
-                <div className="w-[64px] h-[64px] bg-[#FFC451] rounded-[5px] flex justify-center items-center">
-                  <img className="w-[28px] h-[28px]" src={back} alt="" />
-                </div>
-                <h4 className="text-[black] text-[24px] mt-[10px]">Back-end</h4>
-                <p className="text-[#444444] text-[16px] font-normal text-center">
-                  lorem ipsum ipsum lorem lorem ipsum ipsum lorem
-                </p>
-              </div>
-            </div>
-            {/* back-end container */}
-            <div className=" flex items-center justify-center flex-col px-[20px] py-[80px] mt-[40px] border-[2px] md:w-[326px] md:h-[337px] md:mt-0">
-              <div className="w-[100%] h-[175px] flex flex-col justify-center items-center md:w-[294px]">
-                <div className="w-[64px] h-[64px] bg-[#FFC451] rounded-[5px] flex justify-center items-center">
-                  <img className="w-[28px] h-[28px]" src={back} alt="" />
-                </div>
-                <h4 className="text-[black] text-[24px] mt-[10px]">Back-end</h4>
-                <p className="text-[#444444] text-[16px] font-normal text-center">
-                  lorem ipsum ipsum lorem lorem ipsum ipsum lorem
-                </p>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
