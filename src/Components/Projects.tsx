dbba04a69397c94bc9a82b5d98e4fe1bd477f826
David Mikeladze <davidmikeladze00@gmail.com>
2024-04-01 20:52:37 +0400

    feat: add links on photo links

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -69,21 +69,25 @@ function Projects() {
               {/* live links section */}
               <div className="flex">
                 {/* github section */}
-                <div className="middle w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                  <img
-                    src={github}
-                    alt="Github"
-                    className="w-[30px] h-[30px] mx-1"
-                  />
-                </div>
+                <a href={image.github} target="_blank">
+                  <div className="middle w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                    <img
+                      src={github}
+                      alt="Github"
+                      className="w-[30px] h-[30px] mx-1"
+                    />
+                  </div>
+                </a>
                 {/* link section */}
-                <div className="middle ml-[70px] w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
-                  <img
-                    src={link}
-                    alt="Github"
-                    className="w-[30px] h-[30px] mx-1"
-                  />
-                </div>
+                <a href={image.live} target="_blank">
+                  <div className="middle ml-[70px] w-[55px] h-[55px] bg-[black] hover:bg-[#FFC451] rounded-[5px] flex justify-center items-center cursor-pointer">
+                    <img
+                      src={link}
+                      alt="Github"
+                      className="w-[30px] h-[30px] mx-1"
+                    />
+                  </div>
+                </a>
               </div>
             </div>
           ))}
