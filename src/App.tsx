06fe78b751c93156262697e7b3438638172aeb72
David Mikeladze <davidmikeladze00@gmail.com>
2024-04-03 14:50:58 +0400

    feat: add arrow up to scroll when scrolled down window

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Team from "./Components/Team";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import DesktopHeader from "./Components/DesktopHeader";
+import { Link } from "react-scroll";
+import arrowup from "../public/assets/arrow-up.png";
 
 function App() {
   const [hidden, setHidden] = useState<boolean>(false);
@@ -53,6 +55,22 @@ function App() {
         <Team />
         <Contact />
         <Footer />
+        <Link to={"home"} spy={true} smooth={true} offset={0} duration={500}>
+          <div
+            className={`fixed right-[10px] bottom-[10px] circle cursor-pointer w-[40px] h-[40px] hover:bg-[#ffc5515c] duration-300 ease-in-out outline-none bg-[${
+              scrolled ? "#FFC451" : "transparent"
+            }] flex justify-center items-center transition-opacity duration-300 ${
+              scrolled ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ transition: "background-color 0.3s ease-in-out" }}
+          >
+            <img
+              src={arrowup}
+              className="hover:scale-105 transition-all duration-300 w-[25px] h-[25px]"
+              alt="Scroll Up"
+            />
+          </div>
+        </Link>
       </MyContext.Provider>
     </>
   );
